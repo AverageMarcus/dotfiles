5a5fa09e70df057f611af7d3647969807dde73f3
Marcus Noble <github@marcusnoble.co.uk>
2022-06-26 10:00:42 +0100

    Added Viddy
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ BREW_TOOLS=(
   git-delta go helm homebrew/cask-fonts/font-open-dyslexic-nerd-font htop jq kind krew
   kubectl kubectx kustomize node procs progress ripgrep rs/tap/curlie rust starship
   tektoncd/tools/tektoncd-cli tldr tailscale yq hashicorp/tap/vault stats
-  tabby vale lastpass-cli jless macchina tz vmware-tanzu/carvel/kapp
+  tabby vale lastpass-cli jless macchina tz vmware-tanzu/carvel/kapp viddy
   )
 CARGO_TOOLS=( bottom )
 NODE_TOOLS=( git-split-diffs )
