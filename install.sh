071195304d078cfdd5ca5573c99df0a2ac2a5dc1
Marcus Noble <github@marcusnoble.co.uk>
2024-02-20 08:50:40 +0000

    Remove kubectx
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ sudo chmod -R 755 /usr/local/share/zsh/site-functions
 BREW_TOOLS=(
   git argocd bandwhich bat danielfoehrkn/switch/switch derailed/k9s/k9s
   dive doggo duf dust exa fd fzf git-delta go helm htop jq kind krew
-  kubectl kubectx kustomize node procs progress ripgrep rs/tap/curlie rust starship
+  kubectl kustomize node procs progress ripgrep rs/tap/curlie rust starship
   tektoncd/tools/tektoncd-cli tldr tailscale yq hashicorp/tap/vault
   tabby vale lastpass-cli jless macchina tz vmware-tanzu/carvel/kapp viddy
   homeassistant-cli act dnsmasq gh kubebuilder golangci-lint gnu-sed
