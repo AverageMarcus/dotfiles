79fd63ec78df90beaabe2c602f5f5a7311c9774c
Marcus Noble <github@marcusnoble.co.uk>
2024-06-13 08:30:31 +0100

    Added autorise
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ BREW_TOOLS=(
 # Brew tools only available / needed on Mac
 MAC_BREW_TOOLS=(
   pinentry-mac gpg gawk coreutils wget stats homebrew/cask-fonts/font-open-dyslexic-nerd-font
-  hiddenbar
+  hiddenbar dimentium/autoraise/autoraiseapp
   )
 CARGO_TOOLS=( bottom )
 NODE_TOOLS=( git-split-diffs )
