16c864b2304de5bb25686329063dcc291f4ae4bf
Marcus Noble <github@marcusnoble.co.uk>
2025-10-25 09:20:37 +0100

    Added UTM
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ MAC_BREW_TOOLS=(
   dimentium/autoraise/autoraiseapp the-unarchiver rar mas capcut
   mqtt-explorer raycast bettertouchtool calibre kdenlive royal-tsx tableplus
   homebrew/cask/todoist ultimaker-cura webtorrent pika pearcleaner spotmenu
-  jordanbaird-ice
+  jordanbaird-ice utm
   )
 CARGO_TOOLS=( bottom )
 NODE_TOOLS=( git-split-diffs )
